6f05306540f54c8b0f78df14375734b8439d2375
Fernando <fmalca@gmail.com>
2021-12-05 22:35:41 -0500

    Desafío 4 (final)

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -6,25 +6,25 @@ import Button from 'react-bootstrap/Button';
 const ItemCount = ({initial, stock}) => {
     const [count, setCount] = useState(initial)
     
-    const aumenta = () => { 
+    const add = () => { 
         if (count+1 <= stock )       
          setCount(count + 1)   
     }
 
-    const disminuye = () => {
+    const substract = () => {
         if (count-1 >= initial)
         setCount(count - 1)   
     }
 
     return (
         <div>
-            <Button variant="secondary" size="lg" onClick={disminuye} >
+            <Button variant="secondary" size="lg" onClick={substract} >
                 - 
             </Button>{'  '}
             <Button variant="success" size="lg">
             {count}
             </Button>{'  '}
-            <Button variant="secondary" size="lg" onClick={aumenta} >            
+            <Button variant="secondary" size="lg" onClick={add} >            
                 + 
             </Button>
         </div>
