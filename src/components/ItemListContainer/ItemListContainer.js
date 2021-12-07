a67e6f4948e0390f739f26eb17a532648efeb472
Fernando Malca <67565141+concyssa-fmalca@users.noreply.github.com>
2021-12-07 16:19:36 -0500

    Desafio-6

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,8 +1,7 @@
-//import Alert from "react-bootstrap/Alert";
+import Alert from "react-bootstrap/Alert";
 //import ItemCount from "../ItemCount/ItemCount";
 import ItemList from "../ItemList/ItemList";
 import Container from 'react-bootstrap/Container';
-
 import { getFetch } from "../../helpers/getFetch";
 import {useState, useEffect} from "react";
 
@@ -10,15 +9,22 @@ const ItemListContainer = ({greeting}) => {
     //const initial = 1
     //const stock = 5 
     const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true) 
+
 
     useEffect(() => {
         getFetch.then(resp => setProducts(resp))
-    },[])    
+        .catch(err=>console.log(err))
+        .finally(()=>setLoading(false))
+    },[]) 
 
     return (
         <div>
-            <Container fluid >                
-                <ItemList data={products} />                
+            <Container fluid >  
+                               
+                { loading ? <Alert variant="info">Cargando datos ...</Alert>                             
+                            : <ItemList data={products} /> }               
+                              
             </Container>
         </div>
    )
