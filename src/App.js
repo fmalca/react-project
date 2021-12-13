c5c24ce1b24e84801715a314afd0fdf5b9266cfd
Fernando <fmalca@gmail.com>
2021-12-12 23:02:13 -0500

    Desafio-7 Inicial

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import Container from 'react-bootstrap/Container';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cart from './components/Cart/Cart';
 
 
 function App() {
   return (    
     <>
       <Container fluid>
-        <NavBar />
-        <ItemListContainer greeting="Soy el contenedor" />        
-        <ItemDetailContainer  />
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element= {<ItemListContainer greeting="Soy el contenedor" /> } />
+            <Route path="/categoria" element= {<ItemListContainer greeting="Soy el contenedor" /> } />
+            <Route path="/detalle" element= { <ItemDetailContainer  /> } />            
+            <Route path="/cart" element= { <Cart /> } />            
+          </Routes>
+        </BrowserRouter>
       </Container>
     </>
   );
