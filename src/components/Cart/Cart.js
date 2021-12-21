7c2f4b47b4799a03d0db6b39195eff6d45d113db
Fernando Malca <67565141+concyssa-fmalca@users.noreply.github.com>
2021-12-21 15:56:13 -0500

    Desafio-context 100%

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
+import { Button } from 'react-bootstrap'
 import { CartContext } from '../../context/CartContext'
 
 
-const Cart = () => {
-    const {cartList} = useContext(CartContext)   
-
+const Cart = () => {    
+    const {cartList, clear} = useContext(CartContext)    
     return (
         <>
           {cartList.map(
@@ -12,6 +12,14 @@ const Cart = () => {
             <li key={item.id}>{item.title} {' -> ' }{item.quantity}</li>
           )
           }
+
+          {
+            cartList.length ? 
+            <Button onClick={clear} >Vaciar carrito</Button>            
+            : 
+            <p>No hay items seleccionados</p>
+          }
+            
         </>
     )
 }
